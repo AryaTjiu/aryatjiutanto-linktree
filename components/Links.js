06fdaefced2442684c444ce1a0a91be1324a7195
AryaTjiu <aryatjiu.dev@gmail.com>
2024-08-27 20:16:46 +0700

    add new link

--- components/Links.js
+++ components/Links.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { faDiscord, faGithub, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { codewars, github, portofolio, youtube } from '@/data/dataLink'
+import { codewars, github, leetcode, portofolio, youtube } from '@/data/dataLink'
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 import { useHover } from "@/context/HoverContext";
 
@@ -34,11 +34,11 @@ export default function Links(props) {
               </span>
             </div>
           </Link>
-          <Link href={codewars} target='_blank'>
-            <div className='w-full rounded-xl bg-transparent dark:bg-transparent border-2 border-[#181818] dark:border-slate-200 hover:bg-[#181818] hover:text-neutral-200 dark:hover:text-[#181818] dark:hover:bg-slate-200 dark:hover:border-neutral-200 px-5 py-4 text-black dark:text-neutral-200 space-x-3 flex items-center duration-200' onMouseEnter={() => setHovered("Codewars")} onMouseLeave={() => setHovered(false)}>
+          <Link href={leetcode} target='_blank'>
+            <div className='w-full rounded-xl bg-transparent dark:bg-transparent border-2 border-[#181818] dark:border-slate-200 hover:bg-[#181818] hover:text-neutral-200 dark:hover:text-[#181818] dark:hover:bg-slate-200 dark:hover:border-neutral-200 px-5 py-4 text-black dark:text-neutral-200 space-x-3 flex items-center duration-200' onMouseEnter={() => setHovered("Leetcode")} onMouseLeave={() => setHovered(false)}>
               <img src='/leetcode.png' className='w-6'/>
               <span>
-                Codewars
+                Leetcode
               </span>
             </div>
           </Link>
--- data/dataLink.js
+++ data/dataLink.js
@@ -1,5 +1,6 @@
 export const portofolio = "https://portofolio-topaz-two.vercel.app/";
 export const github = "https://github.com/AryaTjiu";
+export const leetcode = "https://leetcode.com/u/aryatjiutanto/";
 export const codewars = "https://www.codewars.com/users/codewithgi";
 export const youtube = "https://www.youtube.com/@codewithgi";
 export const instagram = "https://www.instagram.com/antelocode/";
