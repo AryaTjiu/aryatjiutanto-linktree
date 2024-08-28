0c2ba1c2687487a0feea7718ab8a7b8796024186
AryaTjiu <aryatjiu.dev@gmail.com>
2024-08-28 21:35:31 +0700

    add new link

--- data/dataLink.js
+++ data/dataLink.js
@@ -2,6 +2,7 @@ export const portofolio = "https://aryatjiutanto.vercel.app/";
 export const github = "https://github.com/AryaTjiu";
 export const leetcode = "https://leetcode.com/u/aryatjiutanto/";
 export const codewars = "https://www.codewars.com/users/codewithgi";
+export const fiverr = "https://www.fiverr.com/antelocode";
 export const youtube = "https://www.youtube.com/@codewithgi";
 export const instagram = "https://www.instagram.com/antelocode/";
 export const discord = "https://discord.com/channels/@me/syegi";
